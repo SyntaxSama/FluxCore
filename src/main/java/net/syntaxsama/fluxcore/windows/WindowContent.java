7f017de70504569304e569c566149b54f0e3896b
SyntaxSama <syntaxsama@gmail.com>
2024-11-04 18:28:09 -0500

    Updated the sprite system Added in a boolean check if the sprite exists Added in a remove sprite by id feature

--- src/main/java/net/syntaxsama/fluxcore/windows/WindowContent.java
+++ src/main/java/net/syntaxsama/fluxcore/windows/WindowContent.java
@@ -9,27 +9,30 @@ import javafx.scene.image.ImageView;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WindowContent extends Scene {
 
     private Canvas canvas;
     private GraphicsContext gc;
     private Group root;
-    private ImageView sprite;
+    private Map<String, ImageView> sprites;
 
     public WindowContent(Group root, int width, int height) {
         super(root, width, height);
         this.root = root;
+        this.canvas = new Canvas(width, height);
+        this.gc = canvas.getGraphicsContext2D();
+        this.sprites = new HashMap<>();
 
-        canvas = new Canvas(width, height);
-        gc = canvas.getGraphicsContext2D();
         root.getChildren().add(canvas);
     }
 
-    public void addSprite(String spritePath, double x, double y, double width, double height) {
+    public void addSprite(String id, String spritePath, double x, double y, double width, double height) {
         try {
             Image spriteImage = new Image(new FileInputStream(spritePath));
-            sprite = new ImageView(spriteImage);
+            ImageView sprite = new ImageView(spriteImage);
 
             sprite.setFitWidth(width);
             sprite.setFitHeight(height);
@@ -38,12 +41,24 @@ public class WindowContent extends Scene {
             sprite.setPreserveRatio(true);
 
             root.getChildren().add(sprite);
+            sprites.put(id, sprite);
         } catch (FileNotFoundException e) {
             System.err.println("Something went wrong when trying to add sprite " + spritePath);
         }
     }
 
-    public ImageView getSprite() {
-        return sprite;
+    public ImageView getSprite(String id) {
+        return sprites.get(id);
+    }
+
+    public void removeSpriteById(String id) {
+        ImageView sprite = sprites.remove(id);
+        if (sprite != null) {
+            root.getChildren().remove(sprite);
+        }
+    }
+
+    public boolean hasSprite(String id) {
+        return sprites.containsKey(id);
     }
 }
